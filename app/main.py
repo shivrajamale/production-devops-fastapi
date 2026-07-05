0cbff701a631a348b5f8481facddbb4f9c8fc74d
shivrajamale <shivrajamale2458@gmail.com>
2026-07-05 08:43:16 +0530

    feat: add application logging

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from app.config.config import APP_NAME
 from app.config.config import APP_VERSION
+from app.utils.logger import logger
 
 from app.routers import health
 from app.routers import version
@@ -11,6 +12,8 @@ app = FastAPI(
     title=APP_NAME,
     version=APP_VERSION
 )
+
+logger.info("Production DevOps FastAPI application started")
 
 @app.get("/")
 def home():
--- app/routers/health.py
+++ app/routers/health.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
+from app.utils.logger import logger
 
 router = APIRouter()
 
 @router.get("/health")
 def health():
+    logger.info("Health endpoint called")
     return {
         "status": "healthy"
     }
